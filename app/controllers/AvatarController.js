fb678f2888c392988c7322fedb77c6e31660a588
Jordi Francesc <jfrpla@gmail.com>
2022-05-27 16:37:49 +0200

    Caches Gravatar URL

--- app/controllers/AvatarController.js
+++ app/controllers/AvatarController.js
@@ -3,8 +3,13 @@ import avatarView from "../views/avatar/AvatarView.js";
 
 const LoadAvatarController = async () => {
   const avatarNode = await avatarView();
-  const gravatarUrl = await gravatar("jfrpla@gmail.com");
+
+  const userEmail = "jfrpla@gmail.com";
+  const gravatarUrl =
+    localStorage.getItem(userEmail) || (await gravatar(userEmail));
+
   if (gravatarUrl) {
+    localStorage.setItem(userEmail, gravatarUrl);
     const avatarBox = avatarNode.querySelector(".avatar__content");
     const img = document.createElement("img");
     img.setAttribute("src", gravatarUrl);
