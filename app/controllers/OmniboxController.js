cf4be9877c6a315d02810a2faa06440371d8eb2f
Jordi Francesc <jfrpla@gmail.com>
2022-06-10 18:36:44 +0200

    IDB experiment using callbacks onsucces

--- app/controllers/OmniboxController.js
+++ app/controllers/OmniboxController.js
@@ -1,13 +1,13 @@
 import OmniboxView from "../views/omnibox/OmniboxView.js";
 import { multiSearch } from "../services/api/TMDBApi.js";
 import { throttle } from "../services/RateLimiterService.js";
-
+import { db as DB } from "../main.js";
 
 function view(callback) {
   return async function (...args) {
     const result = await callback.apply(this, args);
     console.log("viewCallback", callback, result);
-  }
+  };
 }
 
 async function querySearch(e) {
@@ -17,7 +17,48 @@ async function querySearch(e) {
 
   console.log(query);
   e.preventDefault();
-  return await multiSearch(query);
+
+  /* Read */
+  async function read(title) {
+    const keyRangeValue = IDBKeyRange.only(title);
+    console.log("keyRangeValue", keyRangeValue);
+    const objectStore = DB.transaction("tmdb_search_results").objectStore(
+      "tmdb_search_results"
+    );
+    const indexRequest = objectStore.index("title").get(keyRangeValue);
+    let results;
+    indexRequest.onsuccess = (e) => {
+      console.log("onSuccess: result", e.target.result);
+      results = e.target.result;
+    };
+    indexRequest.onerror = function (...args) {
+      console.log("onError: this", this);
+      console.log("onError: args", ...args);
+    };
+    console.log("ReSuLTs", results);
+    return results;
+  }
+
+  await read(query);
+
+  /* Write */
+  function write(result) {
+    const newItem = { title: query, results: result.results };
+    const transaction = DB.transaction(["tmdb_search_results"], "readwrite");
+    const objectStore = transaction.objectStore("tmdb_search_results");
+    const addRequest = objectStore.add(newItem);
+    addRequest.addEventListener("success", () => console.log("Search Stored!"));
+    transaction.addEventListener("complete", () =>
+      console.log("Transaction Completed. DB modification finished.")
+    );
+    transaction.addEventListener("error", () =>
+      console.log("Transaction not opened du to error")
+    );
+  }
+  console.log("__________DB____________:", DB);
+  const result = await multiSearch(query);
+
+  return result;
 }
 
 const LoadOmniboxController = async () => {
