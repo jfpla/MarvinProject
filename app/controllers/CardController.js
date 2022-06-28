7edfbed773a7b72e8c68dadbff7aab81eff17448
Jordi Francesc <jfrpla@gmail.com>
2022-06-28 10:25:31 +0200

    card integration

--- app/controllers/CardController.js
+++ app/controllers/CardController.js
@@ -7,18 +7,18 @@ import CardView from "../views/card/CardView.js";
  */
 const LoadCardController = async (data) => {
   const cardTemplate = await CardView();
-  const title = cardTemplate.querySelector(".card__title");
-  const body = cardTemplate.querySelector(".card__body");
-  if (data.media_type === "movie") {
-    title.textContent = data.title;
-    body.textContent = data.overview;
-  } else if (data.media_type === "tv") {
-    title.textContent = data.name;
-    body.textContent = data.overview;
-  } else {
-    title.textContent = data.name;
-    body.textContent = data.known_for_department;
-  }
+  // const title = cardTemplate.querySelector(".card__title");
+  // const body = cardTemplate.querySelector(".card__body");
+  // if (data.media_type === "movie") {
+  //   title.textContent = data.title;
+  //   body.textContent = data.overview;
+  // } else if (data.media_type === "tv") {
+  //   title.textContent = data.name;
+  //   body.textContent = data.overview;
+  // } else {
+  //   title.textContent = data.name;
+  //   body.textContent = data.known_for_department;
+  // }
   return cardTemplate.cloneNode(true).childNodes;
 };
 
