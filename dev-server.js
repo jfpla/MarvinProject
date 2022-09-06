8f9ac95a68e0649ac59e4bb8673bfe529d148278
Jordi Francesc <jfrpla@gmail.com>
2022-09-06 22:10:21 +0200

    FIX use websocket to reload client pt.2 added contentType to the server response

--- dev-server.js
+++ dev-server.js
@@ -15,6 +15,7 @@ import { fileURLToPath } from "url";
 // FIX __dirname: https://bobbyhadz.com/blog/javascript-dirname-is-not-defined-in-es-module-scope
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+console.log("__dirname", __dirname);
 
 const HTTP_PORT = 8089;
 const WEBSOCKET_PORT = 8090;
@@ -25,16 +26,38 @@ const CLIENT_WEBSOCKET_CODE = fs.readFileSync(
 
 // Websocket server (for allowing browser and dev server to have 2-way communication)
 // We don't even need to do anything except create the instance!
-const wss = new WebSocketServer({
+new WebSocketServer({
   port: WEBSOCKET_PORT,
 });
+
+const getContentType = (filePath) => {
+  const extname = String(path.extname(filePath)).toLowerCase();
+  const mimeTypes = {
+    ".html": "text/html",
+    ".js": "text/javascript",
+    ".css": "text/css",
+    ".json": "application/json",
+    ".png": "image/png",
+    ".jpg": "image/jpg",
+    ".gif": "image/gif",
+    ".svg": "image/svg+xml",
+    ".wav": "audio/wav",
+    ".mp4": "video/mp4",
+    ".woff": "application/font-woff",
+    ".ttf": "application/font-ttf",
+    ".eot": "application/vnd.ms-fontobject",
+    ".otf": "application/font-otf",
+    ".wasm": "application/wasm",
+  };
+  return mimeTypes[extname] ?? "application/octet-stream";
+};
 
 /**
  * @typedef {import('http').IncomingMessage} req
  * @typedef {import('http').ServerResponse} res
  */
 
-/** Use classic server-logic to serve a static file (e.g. default to 'index.html' etc)
+/** Use classic server-logic to serve a static file (e.g. default to 'index.html' etc.)
  * @param {string} route
  * @param {res} res
  * @returns {boolean} Whether or not the page exists and was served
@@ -47,7 +70,7 @@ function serveStaticPageIfExists(route, res) {
     if (fs.statSync(route).isDirectory()) {
       return serveStaticPageIfExists(path.join(route, "index.html"), res);
     } else if (fs.statSync(route).isFile()) {
-      res.writeHead(200);
+      res.writeHead(200, { "Content-Type": getContentType(route) });
       /** @type {string|Buffer} */
       let file = fs.readFileSync(route);
       if (route.endsWith(".html")) {
@@ -58,7 +81,7 @@ function serveStaticPageIfExists(route, res) {
         // from *technically correct* HTML.
         file = `${file.toString()}\n\n<script>${CLIENT_WEBSOCKET_CODE}</script>`;
       }
-      res.end(file);
+      res.end(file, "utf-8");
       return true;
     }
   }
@@ -74,7 +97,8 @@ const requestHandler = function (req, res) {
   if (method === "get") {
     // No need to ensure the route can't access other local files,
     // since this is for development only.
-    const route = path.normalize(path.join(__dirname, "src", req.url));
+    const route = path.normalize(path.join(__dirname, req.url));
+    console.log("route: ", route);
     if (serveStaticPageIfExists(route, res)) {
       return;
     }
