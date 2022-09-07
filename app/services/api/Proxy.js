9fe1cfbb4ed385969b5d3c14a97151e375e00f36
Jordi Francesc <jfrpla@gmail.com>
2022-09-07 13:51:46 +0200

    a cloudflare worker cors proxy has been deployed

--- app/services/api/Proxy.js
+++ app/services/api/Proxy.js
@@ -1,5 +1,5 @@
-const url = "https://blooming-depths-86619.herokuapp.com/";
-
+// const url = "https://blooming-depths-86619.herokuapp.com/";
+const url = "https://fragrant-band-3f79.jfpla.workers.dev/?";
 const getURL = (destination) => {
   return url + destination;
 };
