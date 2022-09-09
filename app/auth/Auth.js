bb17ab4dc4afa667ae3916a9945aaf6cc4e39d61
Jordi Francesc <jfrpla@gmail.com>
2022-09-09 18:23:41 +0200

    [WIP] Auth0 integration pt4 extract methods from Auth0 scope

--- app/auth/Auth.js
+++ app/auth/Auth.js
@@ -2,7 +2,6 @@ import UpdateUI from "./UpdateUI.js";
 
 export const login = (auth) => {
   return async () => {
-    console.log("customLogin");
     await auth.loginWithRedirect({
       redirect_uri: window.location.origin,
     });
@@ -15,44 +14,41 @@ export const logout = (auth) => {
   };
 };
 
+const fetchAuthConfig = () => fetch("/auth_config.json");
+
+const configAuthClient = async () => {
+  const response = await fetchAuthConfig();
+  const config = await response.json();
+
+  console.log("auth config: ", config);
+
+  return await createAuth0Client({
+    domain: config.domain,
+    client_id: config.clientId,
+  });
+};
+
+const _auth0 = async () => {
+  const auth0 = await configAuthClient();
+
+  /** UI **/
+  await UpdateUI(auth0);
+
+  const query = window.location.search;
+  if (query.includes("code=") && query.includes("state=")) {
+    await auth0.handleRedirectCallback();
+
+    await UpdateUI(auth0);
+
+    window.history.replaceState({}, document.title, "/");
+  }
+  /** **/
+
+  return auth0;
+};
+
 const Auth0 = async () => {
-  const fetchAuthConfig = () => fetch("/auth_config.json");
-
-  const configAuthClient = async () => {
-    const response = await fetchAuthConfig();
-    const config = await response.json();
-
-    console.log("auth config: ", config);
-
-    return await createAuth0Client({
-      domain: config.domain,
-      client_id: config.clientId,
-    });
-  };
-
   return new Promise((resolve, reject) => {
-    const _auth0 = async () => {
-      const auth0 = await configAuthClient();
-      console.log("auth0 Object", auth0);
-
-      /** UI **/
-      await UpdateUI(auth0);
-
-      const query = window.location.search;
-      console.log("query", query);
-      if (query.includes("code=") && query.includes("state=")) {
-        const res = await auth0.handleRedirectCallback();
-        console.log("RedirectCallback", res);
-
-        await UpdateUI(auth0);
-
-        window.history.replaceState({}, document.title, "/");
-        /** **/
-      }
-
-      return auth0;
-    };
-
     // https://stackoverflow.com/a/27935772
     if (document.readyState === "complete") {
       return resolve(_auth0());
