956ed33f9112d2ea6f4b72f0f1e4334c2bde895d
Jordi Francesc <jfrpla@gmail.com>
2022-09-09 17:58:16 +0200

    [WIP] Auth0 integration pt4 remove unused code

--- app/auth/Auth.js
+++ app/auth/Auth.js
@@ -1,7 +1,3 @@
-// import MainView from "../views/main/MainView.js";
-// import OmniboxController from "../controllers/OmniboxController.js";
-// import AvatarController from "../controllers/AvatarController.js";
-
 import UpdateUI from "./UpdateUI.js";
 
 export const login = (auth) => {
@@ -18,19 +14,6 @@ export const logout = (auth) => {
     auth.logout({ returnTo: window.location.origin });
   };
 };
-
-// const updateUI = async (auth) => {
-//   const isAuthenticated = await auth.isAuthenticated();
-//   console.log("updateUI", isAuthenticated);
-//   document.getElementById("btn-logout").disabled = !isAuthenticated;
-//   document.getElementById("btn-login").disabled = isAuthenticated;
-//   if (isAuthenticated) {
-//     await MainView();
-//     await OmniboxController();
-//     // await NavigatorController();
-//     await AvatarController();
-//   }
-// };
 
 const Auth0 = async () => {
   const fetchAuthConfig = () => fetch("/auth_config.json");
@@ -54,14 +37,6 @@ const Auth0 = async () => {
 
       /** UI **/
       await UpdateUI(auth0);
-      console.log("after updateUI");
-
-      // resolve(auth0);
-
-      // if (auth0.isAuthenticated()) {
-      //   console.log("AUTHENTICATED");
-      //   return;
-      // }
 
       const query = window.location.search;
       console.log("query", query);
@@ -82,36 +57,8 @@ const Auth0 = async () => {
     if (document.readyState === "complete") {
       return resolve(_auth0());
     }
-
     document.addEventListener("DOMContentLoaded", resolve);
-    // window.onload = async () => {
-    //   const auth0 = await configAuthClient();
-    //   console.log("auth0 Object", auth0);
-    //
-    //   /** UI **/
-    //   await updateUI(auth0);
-    //   console.log("after updateUI");
-    //   resolve(auth0);
-    //   // if (auth0.isAuthenticated()) {
-    //   //   console.log("AUTHENTICATED");
-    //   //   return;
-    //   // }
-    //
-    //   const query = window.location.search;
-    //   console.log("query", query);
-    //   if (query.includes("code=") && query.includes("state=")) {
-    //     const res = await auth0.handleRedirectCallback();
-    //     console.log("RedirectCallback", res);
-    //
-    //     await updateUI(auth0);
-    //
-    //     window.history.replaceState({}, document.title, "/");
-    //     /** **/
-    //   }
-    // };
   });
 };
-
-// const Auth = await Auth0();
 
 export default Auth0;
--- app/main.js
+++ app/main.js
@@ -1,7 +1,3 @@
-// import MainView from "./views/main/MainView.js";
-// import AvatarController from "./controllers/AvatarController.js";
-// // import NavigatorController from "./controllers/NavigatorController.js";
-// import OmniboxController from "./controllers/OmniboxController.js";
 import DB from "./services/storage/DB.js";
 import Auth0, { login, logout } from "./auth/Auth.js";
 
@@ -14,9 +10,3 @@ console.log("isAuthenticated", isAuthenticated);
 
 document.getElementById("btn-login").onclick = login(Auth);
 document.getElementById("btn-logout").onclick = logout(Auth);
-// if (isAuthenticated) {
-//   await MainView();
-//   await OmniboxController();
-//   // await NavigatorController();
-//   await AvatarController();
-// }
