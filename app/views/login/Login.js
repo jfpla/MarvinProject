38bca0c902cfb205cc0d5b2120d1f1401ec5bf58
Jordi Francesc <jfrpla@gmail.com>
2022-09-14 13:36:06 +0200

    login: logout and toggleLogin

--- app/views/login/Login.js
+++ app/views/login/Login.js
@@ -1,3 +1,5 @@
+/** login **/
+
 const loginElement = document.querySelector(".login");
 console.log(loginElement);
 
@@ -18,3 +20,44 @@ const loginTransition = (event) => {
 };
 
 loginElement.addEventListener("click", loginTransition);
+
+/** logout **/
+
+const logoutElement = document.querySelector(".logout");
+console.log(logoutElement);
+const logoutTransition = () => {
+  toggleLogin();
+};
+
+logoutElement.addEventListener("click", logoutTransition);
+
+/** toggle login **/
+
+const toggleLogin = () => {
+  const loginLayer = document.querySelector(".bg-image > div:first-of-type");
+  const loginLink = document.querySelector(".bg-image > a:first-of-type");
+
+  const logoutLayer = document.querySelector(".bg-image > div:last-of-type");
+  const logoutLink = document.querySelector(".bg-image > a:last-of-type");
+
+  const isLoginLinkHidden = loginLink.classList.contains("hidden");
+
+  loginLayer.className = "";
+  loginLink.className = "";
+  logoutLayer.className = "";
+  logoutLink.className = "";
+
+  if (isLoginLinkHidden) {
+    loginLayer.classList.add("box", "hidden");
+    loginLink.classList.add("login", "box", "enabled");
+
+    logoutLayer.classList.add("box", "disabled");
+    logoutLink.classList.add("logout", "box", "hidden");
+  } else {
+    logoutLayer.classList.add("box", "hidden");
+    logoutLink.classList.add("logout", "box", "enabled");
+
+    loginLayer.classList.add("box", "disabled");
+    loginLink.classList.add("login", "box", "hidden");
+  }
+};
