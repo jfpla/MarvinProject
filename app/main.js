15c19dc30ed6189d1df317548c70b2e7f496aa64
Jordi Francesc <jfrpla@gmail.com>
2022-09-15 13:21:20 +0200

    environment.example.js

--- app/main.js
+++ app/main.js
@@ -7,6 +7,7 @@ const Auth = await Auth0();
 console.log("Auth", Auth);
 const isAuthenticated = await Auth.isAuthenticated();
 console.log("isAuthenticated", isAuthenticated);
+console.log("User > ", await Auth.getUser());
 
 // document.getElementById("btn-login").onclick = login(Auth);
 // document.getElementById("btn-logout").onclick = logout(Auth);
